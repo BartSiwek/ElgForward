17534b0a422f450d565312d0a712038b6933184e
Bart Siwek <bartlomiej.siwek@gmail.com>
2016-03-06 22:36:16 +0100

    Some initial outline

--- src/trackball_camera.h
+++ src/trackball_camera.h
@@ -1,1 +1,28 @@
 #pragma once
+
+/*
+MODES:
+- Pan
+- Rotate
+- Zoom
+
+EVENTS:
+- On client size changed - register new size
+- On mouse down - call PanStart / RotationStart / ZoomStart
+- On mouse move - call PositionUpdate
+- On mouse up - call PanEnd / RotationEnd / ZoomEnd
+
+Functions:
+- Get model matrix
+- Get projection matrix
+
+Possible members:
+- Mode = enum { PAN, ROTATE, ZOOM }
+- Current view matrix
+- Current projection matrix
+- Start point (of any operation)
+- Current point (of any operation)
+- Additional view matrix (to handle pan and rotate between Start and End)
+- Additional projection matrix (to handle zoom between Start and End)
+
+*/
